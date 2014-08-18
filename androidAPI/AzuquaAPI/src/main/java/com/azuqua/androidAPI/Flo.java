cff9eea4f19d958ab0732fecb8c597e697068073
yintothayang <yintothayang@gmail.com>
2014-08-18 16:38:11 -0700

    Changes to Org and Flo Class.

--- androidAPI/AzuquaAPI/src/main/java/com/azuqua/androidAPI/Flo.java
+++ androidAPI/AzuquaAPI/src/main/java/com/azuqua/androidAPI/Flo.java
@@ -1,23 +1,90 @@
 package com.azuqua.androidAPI;
 
-public class Flo {
-    private final String alias;
-    private final String name;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    public Flo(String alias, String name){
-        this.alias = alias;
+public class Flo implements Parcelable {
+    private String id;
+    private String name;
+    private String alias;
+    private String description;
+    private boolean active;
+
+    public Flo(String id, String name, String alias, String description, Boolean active) {
+        this.id = id;
         this.name = name;
+        this.alias = alias;
+        this.description = description;
+        this.active = active;
     }
 
-    public void invoke() throws  Exception{
-        //   Azuqua.invokeFlo(this.alias);
+    public Flo(Parcel in){
+        String[] stringData = new String[4];
+        boolean[] booleanData = new boolean[1];
+
+        in.readStringArray(stringData);
+        this.id = stringData[0];
+        this.name = stringData[1];
+        this.alias = stringData[2];
+        this.description = stringData[3];
+
+        in.readBooleanArray(booleanData);
+        this.active = booleanData[0];
     }
 
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags){
+        out.writeStringArray(new String[] {
+                this.id,
+                this.name,
+                this.alias,
+                this.description
+        });
+
+        out.writeBooleanArray(new boolean[] {
+                this.active
+        });
+    }
+
+    public static final Creator<Flo> CREATOR = new Creator<Flo>() {
+        public Flo createFromParcel(Parcel in){
+            return new Flo(in);
+        }
+
+        public Flo[] newArray(int size){
+            return new Flo[size];
+        }
+    };
+
+    //Get Id
+    public String getId(){
+        return this.id;
+    }
+
+    //Get Name
+    public String getName(){
+        return this.name;
+    }
+
+    //Get Alias
     public String getAlias(){
         return this.alias;
     }
 
-    public String getName(){
-        return this.name;
+    //Get Description
+    public String getDescription(){
+        return this.description;
     }
+
+    //Get Active
+    public boolean getActive(){
+        return this.active;
+    }
+    
 }
--- androidAPI/AzuquaAPI/src/main/java/com/azuqua/androidAPI/Org.java
+++ androidAPI/AzuquaAPI/src/main/java/com/azuqua/androidAPI/Org.java
@@ -1,27 +1,37 @@
 package com.azuqua.androidAPI;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Org {
     private final String name;
     private final String accessKey;
     private final String accessSecret;
-    private List<Flo> flos;
+    private final ArrayList flos;
 
-    public Org(String name, String accessKey, String accessSecret){
+    public Org(String name, String accessKey, String accessSecret, ArrayList flos){
         this.name = name;
         this.accessKey = accessKey;
         this.accessSecret = accessSecret;
+        this.flos = flos;
     }
 
-    //Get Org's name
+    //Get Name
     public String getName(){
         return this.name;
     }
 
-    public void getFlos(){
-
+    //Get AccessKey
+    public String getAccessKey(){
+        return this.accessKey;
     }
 
+    //Get AccessSecret
+    public String getAccessSecret(){
+        return this.accessSecret;
+    }
 
+    //Get Flos
+    public ArrayList<Flo> getFlos(){
+        return this.flos;
+    }
 }
